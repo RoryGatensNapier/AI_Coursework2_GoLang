2a98e2f81b8e02967477d229fc966333a6d933e5
Rory Gatens <40496042@live.napier.ac.uk>
2021-11-11 15:54:01 +0000

    Added cavern files for initial sets Updated main() to basic test on input1.cav Updated readme Rebuilt

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # AI_Coursework2_GoLang
-A solution to Coursework 2 Option A written in GoLang
+A solution to Coursework 2 Option A (cave routing solver) written in GoLang. Batch file included in main directory to execute the .exe build file
--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 
 	FileHandler "github.com/RoryGatensNapier/AI_Coursework2_GoLang/FileHandler"
 )
 
 func main() {
 	//enter main code here from resources functions
-	fmt.Println(FileHandler.GiveTime())
+	FileHandler.ReadFromFile("./cavernFiles/input1.cav")
 }
