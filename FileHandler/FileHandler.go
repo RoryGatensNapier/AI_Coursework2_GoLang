c6963a232b14ac2023be6e7648200cea43b53e54
Rory Gatens <40496042@live.napier.ac.uk>
2021-11-11 15:11:05 +0000

    Added OpenFile_Read function for opening  - Conditioned to close file when done and log err if file not found

--- FileHandler/FileHandler.go
+++ FileHandler/FileHandler.go
@@ -1,7 +1,22 @@
 package FileHandler
 
-import "time"
+import (
+	"log"
+	"os"
+	"time"
+)
 
 func GiveTime() time.Time {
 	return time.Now()
+}
+
+func OpenFile_Read(filePath string) *os.File {
+	file, err := os.OpenFile(filePath, os.O_RDONLY, 0444)
+	if err != nil {
+		log.Fatal(err)
+		file.Close()
+		return nil
+	}
+	file.Close()
+	return file
 }
