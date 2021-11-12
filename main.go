53b4aa24433edce5a9a1f875865ee9cabcce82e4
Rory Gatens <40496042@live.napier.ac.uk>
2021-11-12 19:52:42 +0000

    Renamed FileHandler alias to FH Changed main test to take data and convert it to ints, then print

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 
-	FileHandler "github.com/RoryGatensNapier/AI_Coursework2_GoLang/FileHandler"
-	matrix "github.com/RoryGatensNapier/AI_Coursework2_GoLang/Math"
+	FH "github.com/RoryGatensNapier/AI_Coursework2_GoLang/FileHandler"
 )
 
 func main() {
 	//enter main code here from resources functions
-	data := FileHandler.ReadFromFile("./cavernFiles/input1.cav")
-	fmt.Println(matrix.Construct(data))
+	data := FH.ReadFromFile("./cavernFiles/input1.cav")
+	FH.ConvertElementsToInt(data)
+	fmt.Println(data)
+	//fmt.Println(m.Construct(data))
 }
