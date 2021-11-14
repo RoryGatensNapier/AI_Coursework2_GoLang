470da109c84c0df29a2814a42e82f98634a73443
Rory Gatens <40496042@live.napier.ac.uk>
2021-11-14 19:39:06 +0000

    Updated MatrixFunctions: - Changed matrix output to now use gathered truths - Shuffled layout - Streamlined Vec2 struct

--- Math/MatrixFunctions.go
+++ Math/MatrixFunctions.go
@@ -4,22 +4,21 @@ package matrix
 import "fmt"
 
 // "log"
-// "strconv"
 
 type Vec2 struct {
-	x float64
-	y float64
+	x, y float64
 }
 
 func ConstructTruthTable(FileData []int) [][]int {
 	dimension := FileData[0]
-	matrix := make([][]int, dimension)
-	for i := 0; i < dimension; i++ {
-		matrix[i] = make([]int, dimension)
-	}
 	locations := FileData[1 : dimension*2]
 	truths := FileData[dimension*2:]
 	fmt.Println("Truths = ", truths)
 	fmt.Println("Locations = ", locations)
+
+	matrix := make([][]int, dimension)
+	for i := 0; i < dimension; i++ {
+		matrix[i] = truths[dimension*i : dimension+dimension*i] //make([]int, dimension)
+	}
 	return matrix
 }
